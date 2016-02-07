3ba80e49441d2cd69d5f8970a939f2c9485bc31b
Rachel Carden <bamadesigner@gmail.com>
2016-02-06 23:25:18 -0600

    Adding a hero image for the inside pages and styling the page title

--- index.php
+++ index.php
@@ -2,21 +2,32 @@
 
 get_header();
 
-?><div id="wpc-content">
-	<div class="row">
-		<div class="large-12 columns"><?php
+?><div id="wpc-main-hero"></div>
+<div id="wpc-main"><?php
 
-			if ( have_posts() ):
-				while ( have_posts() ):
-					the_post();
+	if ( have_posts() ):
+		while ( have_posts() ):
+			the_post();
 
-					the_content();
+			?><div class="wpc-page-title">
+				<div class="row">
+					<div class="large-12 columns">
+						<h1><?php the_title(); ?></h1>
+					</div>
+				</div>
+			</div>
 
-				endwhile;
-			endif;
+			<div id="wpc-content">
+				<div class="row">
+					<div class="large-12 columns"><?php
+						the_content();
+					?></div>
+				</div>
+			</div> <!-- #wpc-content --><?php
 
-		?></div>
-	</div>
-</div> <!-- #wpc-content --><?php
+		endwhile;
+	endif;
+
+?></div> <!-- #wpc-main --><?php
 
 get_footer();
