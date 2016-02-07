37d906ebfad806356004835b51d7b55e7b1acd23
Rachel Carden <bamadesigner@gmail.com>
2016-02-07 14:56:42 -0600

    Moving the page title to inside the main hero area

--- index.php
+++ index.php
@@ -2,32 +2,31 @@
 
 get_header();
 
-?><div id="wpc-main-hero"></div>
-<div id="wpc-main"><?php
+// @TODO decrease size of hero image since height decreased
+if ( have_posts() ):
+	while ( have_posts() ):
+		the_post();
 
-	if ( have_posts() ):
-		while ( have_posts() ):
-			the_post();
-
-			?><div class="wpc-page-title">
+		?><div id="wpc-main-hero">
+			<div class="wpc-page-title">
 				<div class="row">
 					<div class="large-12 columns">
 						<h1><?php the_title(); ?></h1>
 					</div>
 				</div>
 			</div>
-
+		</div>
+		<div id="wpc-main">
 			<div id="wpc-content">
 				<div class="row">
 					<div class="large-12 columns"><?php
 						the_content();
 					?></div>
 				</div>
-			</div> <!-- #wpc-content --><?php
+			</div> <!-- #wpc-content -->
+		</div> <!-- #wpc-main --><?php
 
-		endwhile;
-	endif;
-
-?></div> <!-- #wpc-main --><?php
+	endwhile;
+endif;
 
 get_footer();
