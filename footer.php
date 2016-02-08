5be686f8c0310c5cc670bc6e575c5f532401499d
Rachel Carden <bamadesigner@gmail.com>
2016-02-07 23:42:57 -0600

    Adding the footer, styling block quotes, making the home hero image only 90% high

--- footer.php
+++ footer.php
@@ -1,4 +1,16 @@
-<div id="wpc-footer"></div>
+<div id="wpc-footer">
+	<div class="row">
+		<div class="small-12 columns">
+			<a class="wpc-logo"  href="https://wpcampus.org/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wpcampus-logo-tagline.svg" alt="WPCampus: Where WordPress Meets Higher Education" /></a>
+			<p><strong>WPCampus is a community and conference for those using WordPress in the world of higher education.</strong><br />If you'd like to get involved, <a href="https://wpcampus.org/">visit the WPCampus website</a> for more information.<br />
+			<span class="github-message">This site is powered by <a href="https://wordpress.org/">WordPress</a>. You can view, and contribute to, the theme on <a href="https://github.com/wpcampus/wpcampus-2016-theme">GitHub</a>.</span></p>
+			<p class="icons">
+				<a href="https://twitter.com/wpcampusorg/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter-black.svg" alt="Follow WPCampus on Twitter" /></a>
+				<a href="https://github.com/wpcampus/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/github-black.svg" alt="Follow WPCampus on GitHub" /></a>
+			</p>
+		</div>
+	</div>
+</div> <!-- #wpcampus-footer -->
 
 <?php wp_footer(); ?>
 
