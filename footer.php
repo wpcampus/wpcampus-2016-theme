2c44c5a61dabc48406d04210e658a684bf57fcc3
Rachel Carden <bamadesigner@gmail.com>
2016-03-21 21:12:42 -0500

    Adding Code of Conduct and Contact Us menu items to the footer

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
-<div id="wpc-mailing-list">
+<?php
+
+// Get URL
+$blog_url = get_bloginfo('url');
+
+?><div id="wpc-mailing-list">
 	<div class="row">
 		<div class="large-8 large-centered columns">
 			<h2>Subscribe to WPCampus 2016 News</h2>
@@ -11,7 +16,11 @@
 <div id="wpc-footer">
 	<div class="row">
 		<div class="small-12 columns">
-			<a class="wpc-logo"  href="https://wpcampus.org/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wpcampus-logo-tagline.svg" alt="WPCampus: Where WordPress Meets Higher Education" /></a>
+			<a class="wpc-logo" href="https://wpcampus.org/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/wpcampus-logo-tagline.svg" alt="WPCampus: Where WordPress Meets Higher Education" /></a>
+			<ul class="wpc-footer-menu" role="navigation">
+				<li><a href="<?php echo $blog_url; ?>/code-of-conduct/">Code of Conduct</a></li>
+				<li><a href="<?php echo $blog_url; ?>/contact/">Contact Us</a></li>
+			</ul>
 			<p><strong>WPCampus is a <a href="https://wpcampus.org" title="Learn more about the WPCampus community">community</a> and conference for those using WordPress in the world of higher education.</strong><br />If you'd like to get involved, <a href="https://wpcampus.org/">visit the WPCampus website</a> for more information.<br />
 			<span class="github-message">This site is powered by <a href="https://wordpress.org/">WordPress</a>. You can view, and contribute to, the theme on <a href="https://github.com/wpcampus/wpcampus-2016-theme">GitHub</a>.</span></p>
 			<p class="icons">
