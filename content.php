d76478b583f99ed8caab0f1feb394cfa67911e1b
Rachel Carden <bamadesigner@gmail.com>
2016-04-08 09:44:29 -0700

    Setting blog titles to have post link on home page

--- content.php
+++ content.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header"><?php
-		the_title( '<h2 class="entry-title">', '</h2>' );
+		the_title( '<h2 class="entry-title"><a href="' . get_permalink() . '">', '</a></h2>' );
 	?></header><!-- .entry-header --><?php
 
 	the_content( sprintf(
