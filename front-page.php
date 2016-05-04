58cb44daa793277d8b2c54085113353baf624538
Rachel Carden <bamadesigner@gmail.com>
2016-05-04 00:20:20 -0700

    Optimizing the front page to show excerpts instead of full content

--- front-page.php
+++ front-page.php
@@ -1,5 +1,15 @@
 <?php
 
+// Tweak the excerpt length
+add_filter( 'excerpt_length', function( $excerpt_length ) {
+	return 60;
+});
+
+// Tweak the excerpt more
+add_filter( 'excerpt_more', function( $excerpt_more ) {
+	return ' &hellip;';
+});
+
 get_header();
 
 ?><div id="wpc-home-hero"><?php
@@ -20,9 +30,20 @@ get_header();
 						if ( have_posts() ) :
 							// Start the Loop.
 							while ( have_posts() ) : the_post();
+							
+								?><article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-								get_template_part( 'content', get_post_format() );
-
+									<header class="entry-header"><?php
+										the_title( '<h2 class="entry-title"><a href="' . get_permalink() . '">', '</a></h2>' );
+									?></header><!-- .entry-header --><?php
+								
+									the_excerpt( sprintf(
+										'Continue reading<span class="screen-reader-text"> "%s"</span>',
+										get_the_title()
+									) );
+								
+								?></article><?php
+	
 							endwhile;
 						endif;
 
@@ -41,9 +62,20 @@ get_header();
 						if ( have_posts() ) :
 							// Start the Loop.
 							while ( have_posts() ) : the_post();
+								
+								?><article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-								get_template_part( 'content', get_post_format() );
-
+									<header class="entry-header"><?php
+										the_title( '<h2 class="entry-title"><a href="' . get_permalink() . '">', '</a></h2>' );
+									?></header><!-- .entry-header --><?php
+								
+									the_excerpt( sprintf(
+										'Continue reading<span class="screen-reader-text"> "%s"</span>',
+										get_the_title()
+									) );
+								
+								?></article><?php
+									
 							endwhile;
 						endif;
 					?></div>
