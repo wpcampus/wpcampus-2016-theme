3a641d2efd0803a7c4b2bafab4785f5fea52df13
Rachel Carden <bamadesigner@gmail.com>
2016-07-20 23:52:56 -0700

    Add link to view session details on livestream template

--- template-livestream.php
+++ template-livestream.php
@@ -11,9 +11,10 @@ add_filter( 'the_content', function( $content ) {
 	<script id="wpcampus-2016-livestream-template" type="text/x-handlebars-template">
 		<div id="wpc-ls-event-{{id}}" class="wpc-ls{{#event_types}} {{.}}{{/event_types}}">
 			<h3 class="event-title"><strong>{{title.rendered}}</strong></h3>
-			<div class="callout wpc-ls-callout"><a href="{{session_livestream_url}}">Watch the livestream</a></div>
+			<div class="callout wpc-ls-callout"><a href="{{session_livestream_url}}" target="_blank">Watch the livestream</a></div>
 			{{#if event_speakers}}<div class="event-speakers">{{#each event_speakers}}{{#unless @first}}, {{/unless}}<span class="event-speaker">{{post_title}}</span>{{/each}}</div>{{/if}}
 			<div class="event-dt">{{event_time_display}}</div>
+			<a href="{{link}}">View session details</a>
 			{{#if session_categories}}<div class="event-categories">{{#each session_categories}}{{#unless @first}}, {{/unless}}{{.}}{{/each}}</div>{{/if}}
 			{{#event_links}}{{body}}{{/event_links}}
 			<?php /*<iframe src="{{session_livestream_url}}" style="width:100%;height:600px;"></iframe>*/ ?>
