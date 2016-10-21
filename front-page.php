4e3ede2d8d4628d713cb8c872813f1eed220d834
Rachel Carden <bamadesigner@gmail.com>
2016-10-20 19:14:30 -0700

    Fixing <div> issue

--- front-page.php
+++ front-page.php
@@ -21,12 +21,13 @@ $blog_url = get_bloginfo( 'url' );
 </div>
 <div id="wpc-main">
 	<div id="wpc-content">
-		<div class="row"><?php
+		<div class="row">
+			<?php
 
 			// Print the sidebar
-			if ( is_active_sidebar( 'home-sidebar' ) ) {
+			if ( is_active_sidebar( 'home-sidebar' ) ) { ?>
 
-				?><div class="large-9 columns">
+				<div class="large-9 columns">
 					<div class="wpc-content">
 
 						<div class="callout primary">A <a href="https://2016.wpcampus.org/thank-you/">BIG THANK YOU</a> to all involved for helping make the inaugural WPCampus conference a success!</div>
@@ -49,29 +50,35 @@ $blog_url = get_bloginfo( 'url' );
 						<div class="callout secondary" style="text-align:center;">
 							<h2>Will Your Campus Be WPCampus 2017?</h2>
 							<p><strong><a href="https://wpcampus.org/apply-to-host/">Our hosting application</a> will open Monday, August 22, 2016 and close Friday, October 28, 2016</strong></p>
-							<p>The WPCampus planning team is already hard at work preparing for next year's conference. If you would like your campus to be considered for 2017, please start rounding up any questions you might have and begin&nbsp;talking to campus administrators.</p></div>
+							<p>The WPCampus planning team is already hard at work preparing for next year's conference. If you would like your campus to be considered for 2017, please start rounding up any questions you might have and begin&nbsp;talking to campus administrators.</p>
 						</div>
+						<?php
 
-						<?php if ( have_posts() ) :
+						if ( have_posts() ) :
 							while ( have_posts() ) :
-								the_post(); ?>
-							
+								the_post();
+
+								?>
 								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 									<header class="entry-header">
 										<?php the_title( '<h2 class="entry-title"><a href="' . get_permalink() . '">', '</a></h2>' );?>
 									</header><!-- .entry-header -->
-								
-									<?php the_excerpt( sprintf(
+									<?php
+
+									the_excerpt( sprintf(
 										'Continue reading<span class="screen-reader-text"> "%s"</span>',
 										get_the_title()
-									) ); ?>
-								
-								</article>
-	
-							<?php endwhile;
-						endif; ?>
+									) );
 
+									?>
+								</article>
+								<?php
+
+							endwhile;
+						endif;
+
+						?>
 					</div>
 				</div>
 				<div class="large-3 columns">
@@ -84,33 +91,40 @@ $blog_url = get_bloginfo( 'url' );
 
 				<div class="large-12 columns">
 					<div class="wpc-content">
-						
-						<?php if ( have_posts() ) :
+						<?php
+
+						if ( have_posts() ) :
 							while ( have_posts() ) :
-								the_post(); ?>
-								
+								the_post();
+
+								?>
 								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 									<header class="entry-header">
 										<?php the_title( '<h2 class="entry-title"><a href="' . get_permalink() . '">', '</a></h2>' );?>
 									</header><!-- .entry-header -->
-								
-									<?php the_excerpt( sprintf(
+									<?php
+
+									the_excerpt( sprintf(
 										'Continue reading<span class="screen-reader-text"> "%s"</span>',
 										get_the_title()
-									) ); ?>
-								
+									) );
+
+									?>
 								</article>
-									
-							<?php endwhile;
-						endif; ?>
-						
+								<?php
+
+							endwhile;
+						endif;
+
+						?>
 					</div>
 				</div>
+				<?php
 
-			<?php }
+			}
 
-		?></div>
+		?></div> <!-- .row -->
 	</div> <!-- #wpc-content -->
 
 </div> <!-- #wpc-main --><?php
